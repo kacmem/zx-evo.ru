501e11d2be36847c5380f70b2cf47c7b226670c6
Dryomov Pyotr <dremov.p.v@gmail.com>
2020-06-17 20:12:37 +1000

    build

--- server.bundle.js
+++ server.bundle.js
@@ -15540,7 +15540,7 @@ if (true) {
 /* 121 */
 /***/ (function(module) {
 
-module.exports = JSON.parse("{\"docsSidebars\":{\"someSidebar\":[{\"type\":\"category\",\"label\":\"DocusaurusA\",\"items\":[{\"type\":\"link\",\"label\":\"Style Guide\",\"href\":\"/docs/doc1\"},{\"type\":\"link\",\"label\":\"Document Number 2\",\"href\":\"/docs/doc2\"},{\"type\":\"link\",\"label\":\"This is Document Number 3\",\"href\":\"/docs/doc3\"}]},{\"type\":\"category\",\"label\":\"Features\",\"items\":[{\"type\":\"link\",\"label\":\"Powered by MDX\",\"href\":\"/docs/mdx\"}]}]},\"permalinkToSidebar\":{\"/docs/doc1\":\"someSidebar\",\"/docs/doc2\":\"someSidebar\",\"/docs/doc3\":\"someSidebar\",\"/docs/mdx\":\"someSidebar\"}}");
+module.exports = JSON.parse("{\"docsSidebars\":{\"someSidebar\":[{\"type\":\"category\",\"label\":\"ZX Evolution\",\"items\":[{\"type\":\"link\",\"label\":\"Style Guide\",\"href\":\"/docs/doc1\"},{\"type\":\"link\",\"label\":\"Document Number 2\",\"href\":\"/docs/doc2\"},{\"type\":\"link\",\"label\":\"This is Document Number 3\",\"href\":\"/docs/doc3\"}]},{\"type\":\"category\",\"label\":\"Features\",\"items\":[{\"type\":\"link\",\"label\":\"Powered by MDX\",\"href\":\"/docs/mdx\"}]}]},\"permalinkToSidebar\":{\"/docs/doc1\":\"someSidebar\",\"/docs/doc2\":\"someSidebar\",\"/docs/doc3\":\"someSidebar\",\"/docs/mdx\":\"someSidebar\"}}");
 
 /***/ }),
 /* 122 */
@@ -15789,7 +15789,7 @@ module.exports = JSON.parse("{\"allTagsPath\":\"/blog/tags\",\"slug\":\"facebook
 /* 140 */
 /***/ (function(module) {
 
-module.exports = JSON.parse("{\"docsSidebars\":{\"someSidebar\":[{\"type\":\"category\",\"label\":\"DocusaurusA\",\"items\":[{\"type\":\"link\",\"label\":\"Style Guide\",\"href\":\"/docs/doc1\"},{\"type\":\"link\",\"label\":\"Document Number 2\",\"href\":\"/docs/doc2\"},{\"type\":\"link\",\"label\":\"This is Document Number 3\",\"href\":\"/docs/doc3\"}]},{\"type\":\"category\",\"label\":\"Features\",\"items\":[{\"type\":\"link\",\"label\":\"Powered by MDX\",\"href\":\"/docs/mdx\"}]}]},\"permalinkToSidebar\":{\"/docs/doc1\":\"someSidebar\",\"/docs/doc2\":\"someSidebar\",\"/docs/doc3\":\"someSidebar\",\"/docs/mdx\":\"someSidebar\"},\"isHomePage\":true,\"homePagePath\":\"/docs/doc1\"}");
+module.exports = JSON.parse("{\"docsSidebars\":{\"someSidebar\":[{\"type\":\"category\",\"label\":\"ZX Evolution\",\"items\":[{\"type\":\"link\",\"label\":\"Style Guide\",\"href\":\"/docs/doc1\"},{\"type\":\"link\",\"label\":\"Document Number 2\",\"href\":\"/docs/doc2\"},{\"type\":\"link\",\"label\":\"This is Document Number 3\",\"href\":\"/docs/doc3\"}]},{\"type\":\"category\",\"label\":\"Features\",\"items\":[{\"type\":\"link\",\"label\":\"Powered by MDX\",\"href\":\"/docs/mdx\"}]}]},\"permalinkToSidebar\":{\"/docs/doc1\":\"someSidebar\",\"/docs/doc2\":\"someSidebar\",\"/docs/doc3\":\"someSidebar\",\"/docs/mdx\":\"someSidebar\"},\"isHomePage\":true,\"homePagePath\":\"/docs/doc1\"}");
 
 /***/ }),
 /* 141 */
